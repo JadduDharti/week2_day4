86ec028b221a6f56f117131d340f156740707b5c
JadduDharti <dhartipatel89@gmail.com>
2023-03-23 23:38:37 -0500

    First commit

---
+++ square_module.py
@@ -0,0 +1,12 @@
+from math import pi
+from IPython.display import clear_output
+
+def squareFoot(length, width):
+    clear_output()
+    return "The square foot of house is ", length * width
+
+
+
+def calculate_Cir(radius):
+    clear_output()
+    return (2*pi*radius)
